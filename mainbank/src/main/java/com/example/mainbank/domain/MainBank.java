bb2bc430e64c169d5de94f23298388fccfce152b
murayamatoru <murayama@bronze.ocn.ne.jp>
2024-10-20 16:00:47 +0900

    MainBankRepository追加
    BaseEntity, 継承導入

--- mainbank/src/main/java/com/example/mainbank/domain/MainBank.java
+++ mainbank/src/main/java/com/example/mainbank/domain/MainBank.java
@@ -1,9 +1,28 @@
 package com.example.mainbank.domain;
 
-/**
- * 
- * 
- */
-public class MainBank {
+import jakarta.persistence.Entity;
 
+/**
+ * 取引銀行
+ */
+@Entity
+public class MainBank extends BaseEntity {
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	private Long id;
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+	
+	private String name;
+
+	//コンストラクタ
+	public MainBank() {
+		this.name = "";
+	}
 }
