ba77518a5b84f0abb7544dd494251a7e4176853e
murayamatoru <murayama@bronze.ocn.ne.jp>
2024-11-07 21:20:23 +0900

    銀行分類enumを追加＆永続化まで

--- mainbank/src/main/java/com/example/mainbank/domain/MainBank.java
+++ mainbank/src/main/java/com/example/mainbank/domain/MainBank.java
@@ -6,10 +6,27 @@ import jakarta.persistence.Entity;
  * 取引銀行
  */
 @Entity
-public class MainBank extends BaseEntity {	
+public class MainBank extends BaseEntity {
+	
+	/** 銀行種類 */
+	public enum Category {
+		都市銀行,
+		地方銀行,
+		ネット銀行
+	}
+	
+	/** 銀行名 */
 	private String name;
 	
-	private String bankCategory;
+	/** 銀行種類 */
+	private Category bankCategory;
+
+
+	//コンストラクタ
+	public MainBank(String name, Category category) {
+		this.name = name;
+		this.bankCategory = category;
+	}
 
 	//コンストラクタ
 	public MainBank() {
@@ -24,13 +41,12 @@ public class MainBank extends BaseEntity {
 		this.name = name;
 	}
 
-	public String getBankCategory() {
+	public Category getBankCategory() {
 		return bankCategory;
 	}
 
-	public void setBankCategory(String bankCategory) {
+	public void setBankCategory(Category bankCategory) {
 		this.bankCategory = bankCategory;
 	}
-	
-	
+
 }
