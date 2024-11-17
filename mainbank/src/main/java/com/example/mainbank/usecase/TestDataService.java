17c40d4bba4d2dfc3181fc9b17d9a6fa3a6a5f76
murayamatoru <murayama@bronze.ocn.ne.jp>
2024-11-17 21:36:14 +0900

    BankCategoryのデグレード解決。stringValueのA,B,Cは要検討。

--- mainbank/src/main/java/com/example/mainbank/usecase/TestDataService.java
+++ mainbank/src/main/java/com/example/mainbank/usecase/TestDataService.java
@@ -1,6 +1,7 @@
 package com.example.mainbank.usecase;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,13 +31,23 @@ public class TestDataService {
 	
 	@Transactional
 	public boolean createTestData() {
-		Bank bank1 =  new Bank("三菱UFJ銀行", BankCategory.都銀);
-		this.bankRepository.save(bank1);
+		Bank bk1 =  new Bank("三菱UFJ銀行", BankCategory.都銀);
+		this.bankRepository.save(bk1);
 		
-		MainBank mainBank1 = new MainBank(bank1);
-		mainBank1.setStartDate(LocalDate.now());
-		bank1.addMainBank(mainBank1);
-		this.mainBankRepository.save(mainBank1);
+		MainBank mb1 = new MainBank(bk1);
+		mb1.setStartDate(LocalDate.now());
+		bk1.addMainBank(mb1);
+		this.mainBankRepository.save(mb1);
+		
+		
+		List<Bank> bankList = this.bankRepository.findAll();
+		
+		List<MainBank> mainBankList = this.mainBankRepository.findAll();
+		
+		int sss = bankList.size() + mainBankList.size();
+		
+		var sss2 = BankCategory.MAP;
+		String sss3 = sss2.toString();
 		
 		return true;
 	}
